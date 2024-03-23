25efebd831b0b73d02bf9541a60a31533c2d2230
Joddy Setiawan <92089743+Joddy77@users.noreply.github.com>
2024-03-23 07:22:23 +0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let calculation = '';
+
+    showCalculation();
+
+    function updateCalculation(value) {
+        calculation += value;
+
+        showCalculation();
+
+        localStorage.setItem('calculation', calculation);
+    }
+
+    function showCalculation() {
+          document.querySelector('.js-calculation')
+          .innerHTML = calculation;
+    }
